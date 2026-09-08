a2c56d32213d350ed2de34e024a4fbb73412cfcc
wibem1 <wibem1@googlemail.com>
2026-09-08 18:08:28 +0200

    Bump MusicChatLab version to 1.0.26

--- ui-enhancements.js
+++ ui-enhancements.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='1.0.25';
+const VERSION='1.0.26';
 const TABLET_BREAKPOINT=1180;
 const sidebar=document.getElementById('sidebar');
 const closeButton=document.getElementById('sidebarCloseButton');
