ec0d0e6b29845ec4dde11ec5eea45206d2c6fbf8
wibem1 <wibem1@googlemail.com>
2026-09-08 19:30:33 +0200

    v1.1.5 version

--- ui-enhancements.js
+++ ui-enhancements.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='1.1.4';
+const VERSION='1.1.5';
 const TABLET_BREAKPOINT=1180;
 const sidebar=document.getElementById('sidebar');
 const closeButton=document.getElementById('sidebarCloseButton');
