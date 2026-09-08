ce0b3d0f149b40539cf7ce906fafffc1d838af59
wibem1 <wibem1@googlemail.com>
2026-09-08 19:18:29 +0200

    Bump app version to 1.1.4

--- ui-enhancements.js
+++ ui-enhancements.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='1.1.3';
+const VERSION='1.1.4';
 const TABLET_BREAKPOINT=1180;
 const sidebar=document.getElementById('sidebar');
 const closeButton=document.getElementById('sidebarCloseButton');
