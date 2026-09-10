7eda02f720088e9f9ff02af0d826ccb413974ad4
wibem1 <wibem1@googlemail.com>
2026-09-10 11:16:13 +0200

    Bump Music Chat Lab UI to v1.1.22

--- ui-enhancements.js
+++ ui-enhancements.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='1.1.21';
+const VERSION='1.1.22';
 const TABLET_BREAKPOINT=1180;
 const sidebar=document.getElementById('sidebar');
 const closeButton=document.getElementById('sidebarCloseButton');
